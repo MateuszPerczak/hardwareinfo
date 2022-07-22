17742795670fb88240f748572795cbfb2223b30f
Mateusz Perczak <40388335+MateuszPerczak@users.noreply.github.com>
2022-07-22 09:20:57 +0200

    Fixed app content type

--- src/Components/AppContent/AppContent.tsx
+++ src/Components/AppContent/AppContent.tsx
@@ -1,4 +1,5 @@
-import { FC, useState } from "react";
+import type { FC } from "react";
+import { useState } from "react";
 import GlobalStyle from "../GlobalStyle/GlobalStyle";
 import Nav from "../Nav/Nav";
 import { AnimatePresence } from "framer-motion";
